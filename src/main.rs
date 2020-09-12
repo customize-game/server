24873fdf2fc8ae8bdcd6686dd4f8420f46cd6803
music-brain88 <music.brain88@gmail.com>
2020-09-12 20:15:01 +0900

    add post method

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
+use actix_web::{get, post, App, HttpResponse, HttpServer};
 
 
-#[get("/")]
+struct DataEntry {
+    id: u32,
+    text: String,
+}
+
+struct template {
+    entries: Vec<DataEntry>,
+}
+
+#[post("/")]
 async fn index() ->Result<HttpResponse, actix_web::Error >{
     let response_body = "Customize Game!";
     Ok(HttpResponse::Ok().body(response_body))
 }
 
+#[post("/add")]
+async fn add() ->Result<HttpResponse, actix_web::Error >{
+    let response_body = "Add Post";
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    HttpServer::new(move || App::new().service(index))
+    HttpServer::new(move || 
+    App::new()
+        .service(index)
+        .service(add)
+        )
         .bind("0.0.0.0:5000")?
         .run()
         .await?;
