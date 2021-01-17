70d39509f154ef042e7603ae8e41b29d915558dd
ShassBeleth <shassbeleth@gmail.com>
2021-01-18 01:21:42 +0900

    modify パラメータの各APIについてリクエスト定義　固定値レスポンス定義

--- src/controller/v1/from_manager/parameter.rs
+++ src/controller/v1/from_manager/parameter.rs
@@ -1,71 +1,187 @@
-use actix_web::{web, get, post, put, delete, HttpResponse, Responder};
-use serde::{Serialize, Deserialize};
+use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+use server::service;
 
+// パラメータ取得APIレスポンス
 #[derive(Serialize, Deserialize, Debug)]
-struct DataEntry {
-    id: Option<u32>,
-    text: String,
+struct GetOneResponseEntry {
+    id: u32,            // パラメータID
+    name: String,       // パラメータ名
+    display_order: u32, // 表示順
+    is_deleted: bool,   // 削除済みかどうか
+    version: u32,       // バージョン
 }
-
 // パラメータ取得API
 #[get("/api/v1/manager/parameters/{parameter_id}")]
-pub async fn get_one(web::Path(parameter_id): web::Path<u32>) -> impl Responder {
+pub async fn get_one(
+    web::Path(parameter_id): web::Path<u32>, // パラメータID - パスパラメータ
+) -> impl Responder {
+    // リクエスト取得
     let parameter_id: Option<u32> = Some(parameter_id);
-    let response_body = "get_v1_parameter_from_manager";
-    return HttpResponse::Ok().json(
-        DataEntry {
-            id: parameter_id,
-            text: String::from(response_body),
-        }
-    );
+
+    // データ取得
+    let parameter = service::parameter::find_by_id(parameter_id.unwrap());
+
+    // レスポンス加工
+    return HttpResponse::Ok().json(GetOneResponseEntry {
+        id: parameter.id,
+        name: parameter.name,
+        display_order: parameter.display_order,
+        is_deleted: parameter.is_deleted,
+        version: parameter.version,
+    });
 }
 
+// パラメータ一覧取得APIクエリパラメータ
+#[derive(Deserialize)]
+pub struct GetListRequest {
+    sort_by: Option<u32>, // ソート種別
+    limit: Option<u32>,   // 取得数
+    offset: Option<u32>,  // 取得位置
+}
+// パラメータ一覧取得APIレスポンスのパラメータ
+#[derive(Serialize, Deserialize, Debug)]
+struct ParameterEntryOfGetListResponseEntry {
+    id: u32,            // パラメータID
+    name: String,       // パラメータ名
+    display_order: u32, // 表示順
+    is_deleted: bool,   // 削除済みかどうか
+}
+// パラメータ一覧取得APIレスポンス
+#[derive(Serialize, Deserialize, Debug)]
+struct GetListResponseEntry {
+    total_count: u32,                                      // 合計数
+    parameters: Vec<ParameterEntryOfGetListResponseEntry>, // パラメータ一覧
+}
 // パラメータ一覧取得API
 #[get("/api/v1/manager/parameters")]
-pub async fn get_list() -> impl Responder {
-    let response_body = "get_v1_parameters_from_manager";
-    return HttpResponse::Ok().json(
-        DataEntry {
-            id: Some(999),
-            text: String::from(response_body),
-        }
-    );
+pub async fn get_list(
+    query: web::Query<GetListRequest>, // クエリパラメータ
+) -> impl Responder {
+    // リクエスト取得
+    let sort_by = query.sort_by;
+    let limit = query.limit;
+    let offset = query.offset;
+
+    // データ取得
+    let parameters = service::parameter::find_list(sort_by, limit, offset);
+
+    // レスポンス加工
+    let mut response = GetListResponseEntry {
+        total_count: parameters.total_count,
+        parameters: Vec::new(),
+    };
+    for parameter in &parameters.parameters {
+        response
+            .parameters
+            .push(ParameterEntryOfGetListResponseEntry {
+                id: parameter.id,
+                name: parameter.name.to_string(),
+                display_order: parameter.display_order,
+                is_deleted: parameter.is_deleted,
+            });
+    }
+    return HttpResponse::Ok().json(response);
 }
 
+// パラメータ登録APIリクエスト
+#[derive(Deserialize)]
+pub struct RegisterRequestBody {
+    name: String,       // パラメータ名
+    display_order: u32, // 表示順
+}
+// パラメータ登録APIレスポンス
+#[derive(Serialize, Deserialize, Debug)]
+struct RegisterResponseEntry {
+    id: u32,            // パラメータID
+    name: String,       // パラメータ名
+    display_order: u32, // 表示順
+}
 // パラメータ登録API
 #[post("/api/v1/manager/parameters")]
-pub async fn register() -> impl Responder {
-    let response_body = "register_v1_parameter_from_manager";
-    return HttpResponse::Ok().json(
-        DataEntry {
-            id: Some(999),
-            text: String::from(response_body),
-        }
-    );
+pub async fn register(
+    request_body: web::Json<RegisterRequestBody>, // リクエストボディ
+) -> impl Responder {
+    // リクエスト取得
+    let name = request_body.name.to_string();
+    let display_order = request_body.display_order;
+
+    // データ登録
+    let parameter = service::parameter::register(name, display_order);
+
+    // レスポンス加工
+    return HttpResponse::Ok().json(RegisterResponseEntry {
+        id: parameter.id,
+        name: parameter.name,
+        display_order: parameter.display_order,
+    });
 }
 
+// パラメータ更新APIリクエスト
+#[derive(Deserialize)]
+pub struct UpdateRequestBody {
+    name: String,       // パラメータ名
+    display_order: u32, // 表示順
+    is_deleted: bool,   // 削除済みかどうか
+}
+// パラメータ更新APIレスポンス
+#[derive(Serialize, Deserialize, Debug)]
+struct UpdateResponseEntry {
+    id: u32,            // パラメータID
+    name: String,       // パラメータ名
+    display_order: u32, // 表示順
+    is_deleted: bool,   // 削除済みかどうか
+    version: u32,       // バージョン
+}
 // パラメータ更新API
 #[put("/api/v1/manager/parameters/{parameter_id}")]
-pub async fn update(web::Path(parameter_id): web::Path<u32>) -> impl Responder {
+pub async fn update(
+    web::Path(parameter_id): web::Path<u32>, // パラメータID - パスパラメータ
+    request_body: web::Json<UpdateRequestBody>, // リクエストボディ
+) -> impl Responder {
+    // リクエスト取得
     let parameter_id: Option<u32> = Some(parameter_id);
-    let response_body = "update_v1_parameter_from_manager";
-    return HttpResponse::Ok().json(
-        DataEntry {
-            id: parameter_id,
-            text: String::from(response_body),
-        }
-    );
+    let name = request_body.name.to_string();
+    let display_order = request_body.display_order;
+    let is_deleted = request_body.is_deleted;
+
+    // データ更新
+    let parameter =
+        service::parameter::update(parameter_id.unwrap(), name, display_order, is_deleted);
+    // レスポンス加工
+    return HttpResponse::Ok().json(UpdateResponseEntry {
+        id: parameter.id,
+        name: parameter.name,
+        display_order: parameter.display_order,
+        is_deleted: parameter.is_deleted,
+        version: parameter.version,
+    });
 }
 
+// パラメータ削除APIレスポンス
+#[derive(Serialize, Deserialize, Debug)]
+struct DeleteResponseEntry {
+    id: u32,            // パラメータID
+    name: String,       // パラメータ名
+    display_order: u32, // 表示順
+    is_deleted: bool,   // 削除済みかどうか
+    version: u32,       // バージョン
+}
 // パラメータ削除API
 #[delete("/api/v1/manager/parameters/{parameter_id}")]
 pub async fn delete(web::Path(parameter_id): web::Path<u32>) -> impl Responder {
+    // リクエスト取得
     let parameter_id: Option<u32> = Some(parameter_id);
-    let response_body = "delete_v1_parameter_from_manager";
-    return HttpResponse::Ok().json(
-        DataEntry {
-            id: parameter_id,
-            text: String::from(response_body),
-        }
-    );
+
+    // データ削除
+    let parameter = service::parameter::delete(parameter_id.unwrap());
+
+    // レスポンス加工
+    return HttpResponse::Ok().json(DeleteResponseEntry {
+        id: parameter.id,
+        name: parameter.name,
+        display_order: parameter.display_order,
+        is_deleted: parameter.is_deleted,
+        version: parameter.version,
+    });
 }
---
+++ src/service/parameter.rs
@@ -0,0 +1,83 @@
+pub struct ParameterEntry {
+  pub id: u32,
+  pub name: String,
+  pub display_order: u32,
+  pub is_deleted: bool,
+  pub version: u32,
+}
+
+pub struct ParameterTemplate {
+  pub total_count: u32,
+  pub parameters: Vec<ParameterEntry>,
+}
+
+pub fn find_by_id(_id: u32) -> ParameterEntry {
+  return ParameterEntry {
+    id: 4,
+    name: String::from("HP"),
+    display_order: 1,
+    is_deleted: false,
+    version: 0,
+  };
+}
+
+pub fn find_list(
+  _sort_by: Option<u32>,
+  _limit: Option<u32>,
+  _offset: Option<u32>,
+) -> ParameterTemplate {
+  return ParameterTemplate {
+    total_count: 340,
+    parameters: vec![
+      ParameterEntry {
+        id: 4,
+        name: String::from("HP"),
+        display_order: 1,
+        is_deleted: false,
+        version: 0,
+      },
+      ParameterEntry {
+        id: 4,
+        name: String::from("HP"),
+        display_order: 1,
+        is_deleted: false,
+        version: 0,
+      },
+    ],
+  };
+}
+
+pub fn register(_name: String, _display_order: u32) -> ParameterEntry {
+  return ParameterEntry {
+    id: 4,
+    name: String::from("HP"),
+    display_order: 1,
+    is_deleted: false,
+    version: 0,
+  };
+}
+
+pub fn update(
+  _id: u32,
+  _name: String,
+  _display_order: u32,
+  _is_deleted: bool,
+) -> ParameterEntry {
+  return ParameterEntry {
+    id: 4,
+    name: String::from("HP"),
+    display_order: 1,
+    is_deleted: false,
+    version: 0,
+  };
+}
+
+pub fn delete(_id: u32) -> ParameterEntry {
+  return ParameterEntry {
+    id: 4,
+    name: String::from("HP"),
+    display_order: 1,
+    is_deleted: false,
+    version: 0,
+  };
+}
