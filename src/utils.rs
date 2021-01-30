04e599f3d832c8310341fbdec23e514450a4b936
music-brain88 <music.brain88@gmail.com>
2021-01-30 23:26:16 +0900

    Add server config setting struct

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,28 @@
 use diesel::mysql::MysqlConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
+use config::ConfigError;
+
+pub struct Config {
+    pub db_user: String,
+    pub db_password: String,
+    pub db_name: String,
+    pub db_port: i32,
+    pub server_port: i32,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self { 
+            db_user: "root".to_string(),
+            db_password: "root".to_string(),
+            db_name: "player_info".to_string(),
+            db_port: 3306,
+            server_port: 5000
+        }
+    }
+}
+
 
 pub fn establish_connection() -> MysqlConnection {
     let database_url = "mysql://root:root@localhost:3306/player_info";
