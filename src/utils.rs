70e8d2a4c18fb14344a3c0b54240700da4ce8eec
1saver <43394582+music-brain88@users.noreply.github.com>
2021-02-24 22:45:23 +0900

    Feature/use envfile (#35)
    * Add server config setting struct
    
    * Add serde, lazy_static
    
    * update actix-web
    
    * move sql file to initdb.d
    
    * remove sql file
    
    * load env
    
    * Modified indent
    
    * for got commit
    
    * docker-compose suppoer .env file
    
    * server support use .env file
    
    * modified db name
    
    * Add todo comment Close #33, Close #7,Close #3

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,57 @@
 use diesel::mysql::MysqlConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
+use config::Environment;
+use config::ConfigError;
+use serde::Deserialize;
+use lazy_static::lazy_static;
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub db_user: String,
+    pub db_password: String,
+    pub db_name: String,
+    pub db_port: i32,
+    pub server_port: i32,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self { 
+            db_user: "root".to_string(),
+            db_password: "root".to_string(),
+            db_name: "player_info".to_string(),
+            db_port: 3306,
+            server_port: 5000
+        }
+    }
+}
+
+impl Config {
+    pub fn get_env_info() -> Result<Self, ConfigError> {
+        let mut config = config::Config::new();
+        config.merge(config::Environment::new())?;
+        config.try_into()
+    }
+}
+
+
+// TODO: separate SERVER and DB ENV VALUES
+lazy_static! {
+    pub static ref CONFIG: Config = {
+        dotenv().ok();
+        Config::get_env_info().unwrap()
+    };
+}
+
 
 pub fn establish_connection() -> MysqlConnection {
-    let database_url = "mysql://root:root@localhost:3306/player_info";
+    let database_url = format!("mysql://{}:{}@db:{}/{}", 
+                               CONFIG.db_user,
+                               CONFIG.db_password,
+                               CONFIG.db_port,
+                               CONFIG.db_name
+                               );
     MysqlConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
