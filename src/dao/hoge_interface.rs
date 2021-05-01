6a33ea1ca34e6f82377cffb060c2f67d6fa45961
ShassBeleth <shassbeleth@gmail.com>
2021-05-01 15:30:48 +0900

    modify インタフェースのマスタ操作API作成（雑）

---
+++ src/dao/hoge_interface.rs
@@ -0,0 +1,158 @@
+use diesel::deserialize::QueryableByName;
+use diesel::mysql::Mysql;
+use diesel::prelude::*;
+use diesel::sql_query;
+use diesel::sql_types::Integer;
+use diesel::sql_types::Varchar;
+
+#[derive(Debug, Queryable)]
+pub struct HogeInterface {
+  pub id: i32,            // hogeインタフェースID
+  pub name: String,       // hogeインタフェース名
+  pub display_order: i32, // 表示順
+  pub version: i32,       // バージョン
+}
+impl QueryableByName<Mysql> for HogeInterface {
+  fn build<R: diesel::row::NamedRow<Mysql>>(row: &R) -> diesel::deserialize::Result<Self> {
+    return Ok(HogeInterface {
+      id: row.get("id")?,
+      name: row.get("name")?,
+      display_order: row.get("display_order")?,
+      version: row.get("version")?,
+    });
+  }
+}
+// hogeインタフェース取得
+pub fn find_by_id(
+  _connection: &diesel::MysqlConnection, // 接続情報
+  _id: i32,                              // hogeインタフェースID
+) -> Result<Vec<HogeInterface>, diesel::result::Error> {
+  let result: Result<Vec<HogeInterface>, diesel::result::Error> = sql_query(
+    "SELECT
+      h.id ,
+      h.name ,
+      h.display_order,
+      h.version
+    FROM
+      hoge_interfaces h
+    WHERE
+      h.is_deleted = 0
+    AND
+      h.id = ?
+    ",
+  )
+  .bind::<Integer, _>(_id)
+  .load(_connection);
+  return result;
+}
+
+// hogeインタフェース一覧取得
+pub fn find_list(
+  _connection: &diesel::MysqlConnection, // 接続情報
+) -> Result<Vec<HogeInterface>, diesel::result::Error> {
+  let result: Result<Vec<HogeInterface>, diesel::result::Error> = sql_query(
+    "SELECT
+      h.id ,
+      h.name ,
+      h.display_order,
+      h.version
+    FROM
+      hoge_interfaces h
+    WHERE
+      h.is_deleted = 0
+    ",
+  )
+  .load(_connection);
+  return result;
+}
+
+// hogeインタフェース登録
+// 
+// TODO hoge_interfaces.idにauto_incrementが必要
+// alter table hoge_interfaces modify id int auto_increment;
+// 
+pub fn register(
+  _connection: &diesel::MysqlConnection, // 接続情報
+  _name: String, // hogeインタフェース名
+  _display_order: i32, // 表示順
+) -> Result<usize, diesel::result::Error> {
+  let result: Result<usize, diesel::result::Error> = sql_query(
+    "INSERT INTO
+      hoge_interfaces (
+        name ,
+        display_order ,
+        is_deleted ,
+        created_datetime ,
+        updated_datetime ,
+        version
+      )
+      VALUES(
+        ? ,
+        ? ,
+        0 ,
+        now() ,
+        now() ,
+        0 
+      )
+    ",
+  )
+  .bind::<Varchar, _>(_name)
+  .bind::<Integer, _>(_display_order)
+  .execute(_connection);
+  return result;
+}
+
+// hogeインタフェース更新
+pub fn update(
+  _connection: &diesel::MysqlConnection, // 接続情報
+  _id: i32, // hogeインタフェースID
+  _name: String, // hogeインタフェース名
+  _display_order: i32, // 表示順
+  _version: i32, // バージョン
+) -> Result<usize, diesel::result::Error> {
+  let result: Result<usize, diesel::result::Error> = sql_query(
+    "UPDATE
+      hoge_interfaces
+    SET
+      name = ? ,
+      display_order = ? ,
+      updated_datetime = now() ,
+      version = version + 1
+    WHERE
+      id = ?
+    AND
+      version = ?
+    ",
+  )
+  .bind::<Varchar, _>(_name)
+  .bind::<Integer, _>(_display_order)
+  .bind::<Integer, _>(_id)
+  .bind::<Integer, _>(_version)
+  .execute(_connection);
+  return result;
+}
+
+// hogeインタフェース削除
+pub fn delete(
+  _connection: &diesel::MysqlConnection, // 接続情報
+  _id: i32, // hogeインタフェースID
+  _version: i32, // バージョン
+) -> Result<usize, diesel::result::Error> {
+  let result: Result<usize, diesel::result::Error> = sql_query(
+    "UPDATE
+      hoge_interfaces
+    SET
+      is_deleted = 1 ,
+      updated_datetime = now() ,
+      version = version + 1
+    WHERE
+      id = ?
+    AND
+      version = ?
+    ",
+  )
+  .bind::<Integer, _>(_id)
+  .bind::<Integer, _>(_version)
+  .execute(_connection);
+  return result;
+}
--- src/service/hoge_interface.rs
+++ src/service/hoge_interface.rs
@@ -1,107 +1,126 @@
+use crate::dao;
+
+use diesel::connection::Connection;
+use diesel::result::Error;
+
+use crate::utils::establish_connection;
+
 // hogeインタフェース
 pub struct HogeInterfaceEntry {
-  pub id: u32,            // hogeインタフェースID
+  pub id: i32,            // hogeインタフェースID
   pub name: String,       // hogeインタフェース名
-  pub display_order: u32, // 表示順
-  pub is_deleted: bool,   // 削除済みかどうか
-  pub version: u32,       // バージョン
+  pub display_order: i32, // 表示順
+  pub version: i32,       // バージョン
 }
 
 // hogeインタフェース一覧
 pub struct HogeInterfaceTemplate {
-  pub total_count: u32,                         // 合計数
+  pub total_count: usize,                         // 合計数
   pub hoge_interfaces: Vec<HogeInterfaceEntry>, // hogeインタフェース一覧
 }
 
 // hogeインタフェース取得
-pub fn find_by_id(_id: u32, // hogeインタフェースID
-) -> HogeInterfaceEntry {
-  // データ取得
+pub fn find_by_id(_id: i32, // hogeインタフェースID
+) -> Result<HogeInterfaceEntry, Error> {
+  let connection = establish_connection();
+  return connection.transaction::<HogeInterfaceEntry, _, _>(|| {  
 
-  // データ加工
-  return HogeInterfaceEntry {
-    id: _id,
-    name: String::from("右手"),
-    display_order: 1,
-    is_deleted: false,
-    version: 0,
-  };
+    // データ取得
+    let result = dao::hoge_interface::find_by_id(&connection, _id ).unwrap();
+    let hoge_interface = result.first().unwrap();
+
+    // データ加工
+    return Ok(HogeInterfaceEntry{
+      id: hoge_interface.id,
+      name: hoge_interface.name.to_string(),
+      display_order: hoge_interface.display_order,
+      version: hoge_interface.version,
+    });
+
+  });
 }
 
 // hogeインタフェース一覧取得
 pub fn find_list(
-  _sort_by: Option<u32>, // ソート種別
-  _limit: Option<u32>,   // 取得数
-  _offset: Option<u32>,  // 取得位置
-) -> HogeInterfaceTemplate {
-  // データ取得
+  _sort_by: Option<i32>, // ソート種別
+  _limit: Option<i32>,   // 取得数
+  _offset: Option<i32>,  // 取得位置
+) -> Result<HogeInterfaceTemplate, Error> {
+  let connection = establish_connection();
+  return connection.transaction::<HogeInterfaceTemplate, _, _>(|| {
 
-  // データ加工
-  return HogeInterfaceTemplate {
-    total_count: 340,
-    hoge_interfaces: vec![
-      HogeInterfaceEntry {
-        id: 1,
-        name: String::from("右手"),
-        display_order: 1,
-        is_deleted: false,
-        version: 0,
-      },
-      HogeInterfaceEntry {
-        id: 2,
-        name: String::from("右手"),
-        display_order: 1,
-        is_deleted: false,
-        version: 0,
-      },
-    ],
-  };
+    // データ取得
+    let result = dao::hoge_interface::find_list(&connection).unwrap();
+
+    // データ加工
+    return Ok(HogeInterfaceTemplate{
+      total_count: result.len() ,
+      hoge_interfaces: result.iter().map(|hoge_interface| HogeInterfaceEntry {
+        id: hoge_interface.id,
+        name: hoge_interface.name.to_string(),
+        display_order: hoge_interface.display_order,
+        version: hoge_interface.version,
+      })
+      .collect(),
+    });
+  });
 }
 
 // hogeインタフェース登録
 pub fn register(
   _name: String,       // hogeインタフェース名
-  _display_order: u32, // 表示順
-) -> HogeInterfaceEntry {
-  // データ登録
+  _display_order: i32, // 表示順
+) -> Result<usize, Error> {
+  let connection = establish_connection();
+  return connection.transaction::<usize, _, _>(|| {
+    // データ登録
+    let result = dao::hoge_interface::register(
+      &connection,
+      _name,
+      _display_order
+    );
+    // データ加工
+    return Ok(result.unwrap());
 
-  // データ加工
-  return HogeInterfaceEntry {
-    id: 2,
-    name: String::from("右手"),
-    display_order: 1,
-    is_deleted: false,
-    version: 0,
-  };
+  });
 }
 
 // hogeインタフェース更新
 pub fn update(
-  _id: u32,            // hogeインタフェースID
+  _id: i32,            // hogeインタフェースID
   _name: String,       // hogeインタフェース名
-  _display_order: u32, // 表示順
-  _is_deleted: bool,   // 削除済みかどうか
-) -> HogeInterfaceEntry {
-  // データ更新
-
-  // データ加工
-  return HogeInterfaceEntry {
-    id: 2,
-    name: String::from("右手"),
-    display_order: 1,
-    is_deleted: false,
-    version: 0,
-  };
+  _display_order: i32, // 表示順
+  _version: i32,       // バージョン
+) -> Result<usize,Error> {
+  let connection = establish_connection();
+  return connection.transaction::<usize, _, _>(|| {
+    // データ更新
+    let result = dao::hoge_interface::update(
+      &connection,
+      _id,
+      _name,
+      _display_order,
+      _version
+    );
+    // データ加工
+    return Ok(result.unwrap());
+  });
 }
 
 // hogeインタフェース削除
-pub fn delete(_id: u32, // hogeインタフェースID
-) -> HogeInterfaceEntry {
-  return HogeInterfaceEntry {
-    id: 2,
-    name: String::from("右手"),
-    display_order: 1,
-    is_deleted: false,
-    version: 0,
-  };
+pub fn delete(
+  _id: i32,      // hogeインタフェースID
+  _version: i32, // バージョン
+) -> Result<usize,Error> {
+  let connection = establish_connection();
+  return connection.transaction::<usize, _, _>(|| {
+    // データ更新
+    let result = dao::hoge_interface::delete(
+      &connection,
+      _id,
+      _version
+    );
+    // データ加工
+    return Ok(result.unwrap());
+  });
 }
